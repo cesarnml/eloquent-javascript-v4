4f7a1afef8ff64a261d272a0456baa8506f124dc
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-04-24 11:31:54 +0700

    setup exercise prompts

---
+++ exercises/chapter03/minimum.js
@@ -0,0 +1,3 @@
+/**
+ * The previous chapter introduced the standard function Math.min that returns its smallest argument. We can write a function like that ourselves now. Define the function min that takes two arguments and returns their minimum.
+ */
