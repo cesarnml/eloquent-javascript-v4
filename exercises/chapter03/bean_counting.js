c9017b2c425bbb9596986802a765a3d4b2b91880
Cesar Napoleon Mejia Leiva <cmejia@gmail.com>
2024-04-24 12:06:34 +0700

    complete chapter03 exercises

--- exercises/chapter03/bean_counting.js
+++ exercises/chapter03/bean_counting.js
@@ -1,7 +1,50 @@
-/**
+mpl; /**
  * You can get the Nth character, or letter, from a string by writing [N] after the string (for example, string[2]). The resulting value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
 Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase B characters there are in the string.
 
 Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase B characters). Rewrite countBs to make use of this new function.
  */
+
+function countChar(string, target) {
+  let count = 0;
+  for (let char of string) {
+    if (char === target) count++;
+  }
+  return count;
+}
+
+function countBs(string) {
+  // for (let char of string) {
+  //   if (char === "B") count++;
+  // }
+  return countChar(string, "B");
+}
+
+console.log(countBs("BOB"));
+// → 2
+console.log(countChar("kakkerlak", "k"));
+// → 4
+
+/**
+ * Textbook solution
+ */
+
+function countChar_(string, ch) {
+  let counted = 0;
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] == ch) {
+      counted += 1;
+    }
+  }
+  return counted;
+}
+
+function countBs_(string) {
+  return countChar_(string, "B");
+}
+
+console.log(countBs_("BBC"));
+// → 2
+console.log(countChar_("kakkerlak", "k"));
+// → 4
--- exercises/chapter03/recursion.js
+++ exercises/chapter03/recursion.js
@@ -11,3 +11,32 @@ Define a recursive function isEven corresponding to this description. The functi
 
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
  */
+
+function isEven(n) {
+  if (n < 0) return "Enter a positive value to test.";
+  if (n === 0) return true;
+  if (n === 1) return false;
+  return isEven(n - 2);
+}
+
+console.log(isEven(50));
+// → true
+console.log(isEven(75));
+// → false
+console.log(isEven(-1));
+// → ??
+
+/**
+ * Textbook solution
+ */
+function isEven(n) {
+  if (n == 0) return true;
+  else if (n == 1) return false;
+  else if (n < 0) return isEven(-n);
+  else return isEven(n - 2);
+}
+
+/**
+ * My comment:
+ * Huh. It seemed that negative numbers were out of scope from the prompt, but if I was told to solve for it I definitely could have done so.
+ */
